5b3d3ba784d57b12a62b44566242642865311e1b
Alexleru <alexeymoblive@gmail.com>
2018-12-28 23:51:39 +0300

    lesson7

---
+++ baseOfSwiftLesson7/baseOfSwiftLesson7/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  baseOfSwiftLesson7
+//
+//  Created by Darya Bodaykina on 28.12.2018.
+//  Copyright © 2018 alexleru. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
